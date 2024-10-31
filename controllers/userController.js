b201d3bf8f050e05b5766fc8da4a0f116c8fd9c5
To Nguyen Bao <tobao97@gmail.com>
2024-10-31 21:05:00 +0700

    Get User Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -129,7 +129,17 @@ const logoutUser = asyncHandler(async (req,res) => {
 })
 
 const getUser = asyncHandler(async (req,res) => {
-  res.send('Get User')
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    const {_id, name, email, phone, bio, photo, role, isVerified} = user
+    res.status(200).json({
+      _id, name, email, phone, bio, photo, role, isVerified
+    })
+  } else{
+    res.status(404)
+    throw new Error('User not found...')
+  }
 })
 
 module.exports = {
