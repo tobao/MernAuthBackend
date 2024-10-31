c198986bc0120951c2f7a6dc41cb0e488dbe4573
To Nguyen Bao <tobao97@gmail.com>
2024-10-31 19:04:37 +0700

    Register User Validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,42 @@
-const registerUser = async (req, res) => {
-  res.send('Register user');
-};
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+const bcrypt = require('bcryptjs')
+
+const registerUser = asyncHandler(async (req, res) => {
+  //Lấy các thông tin cần thiết từ body của request. Đây là các thông tin được người dùng cung cấp khi đăng ký.
+  const { name, email, password } = req.body
+
+  //Validation 
+  if (!name || !email || !password) {
+    //Kiểm tra xem tất cả các trường name, email, và password có được cung cấp hay không. Nếu thiếu trường nào, trả về lỗi 400.
+    res.status(400)
+    throw new Error('Please fill in all the required fields ')
+  }
+
+  if(password.length <6){
+    //Kiểm tra độ dài của mật khẩu phải ít nhất là 6 ký tự.
+    res.status(400)
+    throw new Error('Password mush be up to 6 characters')
+  }
+
+  //CHeck if user exists - Tìm kiếm người dùng với email đã cung cấp. Nếu người dùng đã tồn tại, trả về lỗi 400.
+  const userExists = await User.findOne({email})
+
+  if(userExists){
+    res.status(400)
+    throw new Error('Email already in use')
+  }
+
+  //Create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+
+
+})
 
 module.exports = {
   registerUser
