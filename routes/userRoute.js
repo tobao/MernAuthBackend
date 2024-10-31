6ca829ce01d83efeec2ccb034d95fd22e82c6622
To Nguyen Bao <tobao97@gmail.com>
2024-10-31 20:24:52 +0700

    Setup Login Controller

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@ const { registerUser, loginUser } = require("../controllers/userController")
 const router = express.Router();
 
 router.post('/register', registerUser)
-router.post('/login', loginUser)
+router.get('/login', loginUser)
 
 
 module.exports = router;
